8b3ac79ef67850e241eb3a8b1268ecb515b55c36
phper95 <1366295050@qq.com>
2023-02-23 08:16:17 +0800

    update

--- demo/4-10/make-new/main.go
+++ demo/4-10/make-new/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unsafe"
+)
 
+func main() {
+	var a1 [100]int
+	var a2 [100]struct{}
+	fmt.Println("a1:", unsafe.Sizeof(a1), "a2:", unsafe.Sizeof(a2))
 }
