31ad976e85d42b79ce3a4b1e1c462e79702ef30a
phper95 <1366295050@qq.com>
2023-02-26 22:35:32 +0800

    4-10

--- demo/4-10/make-new/main.go
+++ demo/4-10/make-new/main.go
@@ -3,40 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	//声明切片slice1并申请地址，为申请的10个元素都赋上元素类型的零值
+	var slice1 = make([]int, 10)
+	fmt.Println(slice1, len(slice1), cap(slice1)) //10 10
 
-	//var a1 *[]int = new([]int) // allocates slice structure; *p == nil; rarely useful
-	//
-	//var a2 []int = make([]int, 0) // the slice v now refers to a new array of 100 ints
-	//fmt.Println(*a1 == nil)
-	//fmt.Println(a2 == nil)
-	//fmt.Println("a1:", a1, "a2:", a2)
-	//fmt.Println("a1:", unsafe.Sizeof(a1), "a2:", unsafe.Sizeof(a2))
+	s1 := make([]int, 0)
+	fmt.Println("s1", len(s1), cap(s1))
+	s2 := new([]int)
+	s3 := *new([]int)
+	var s4 []int
 
-	//var a1 [100]int
-	//var a2 [100]struct{}
-	//fmt.Println("a1:", unsafe.Sizeof(a1), "a2:", unsafe.Sizeof(a2))
+	fmt.Println("s1 is nil?", s1 == nil)  //false
+	fmt.Println("s2 is nil?", *s2 == nil) //true
+	fmt.Println("s3 is nil?", s3 == nil)  //true
+	fmt.Println("s4 is nil?", s4 == nil)  //true
 
-	//通过make申请
-	var s1 = make([]int, 100)
-
-	//初始化第100个元素
-	var s2 = []int{99: 0}
-
-	//先创建一个容量为100的数组再截取全部元素作为切片
-	var s3 = (&[100]int{})[:]
-	var s4 = new([100]int)[:]
-	// 100 100 100 100
-	println(len(s1), len(s2), len(s3), len(s4))
-
-	var s5 *[]int //为nil，直接使用会报空指针
-	s6 := make([]int, 1)
-	s5 = &s6
-	s6 = append(s6, 1)
-	fmt.Println("s5:", s5)
+	a1 := *new([10]int)
+	a2 := [10]int{}
+	fmt.Println(a1, a2)
 }
-
-//对于使用大量堆栈的应用程序来说，从小的堆栈开始是低效的，因为应用程序必须在每个goroutine堆栈变得可用之前增加它。因此，如果我们知道我们将需要这些空间，那么最好从大的堆栈开始。但是我们要避免在开始时分配太大的堆栈，否则我们会浪费太多的空间，或者花时间不必要地缩小堆栈。
-//
-//堆栈开始时是最小的要求大小（通常是2KB）。
-//当堆栈溢出时，我们分配一个两倍大的新堆栈，并将堆栈内容复制到新的分配中。
-//在GC时间，如果一个堆栈的使用量少于分配的堆栈的1/4，我们就分配一个一半大小的新堆栈，并将堆栈内容复制过去。
