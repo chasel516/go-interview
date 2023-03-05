d7ed33c3083a8ef20c3235cfe81022473f6cd94c
phper95 <1366295050@qq.com>
2023-03-06 07:29:08 +0800

    update

---
+++ demo/4-20/nil/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	var i *int = nil
+	fmt.Println("i.size:", unsafe.Sizeof(i)) //8
+
+	var i8 *int8 = nil
+	fmt.Println("i8.size:", unsafe.Sizeof(i8)) //8
+
+	var s *string = nil
+	fmt.Println("s.size:", unsafe.Sizeof(s)) //8
+
+	var ps *struct{} = nil
+	fmt.Println("ps.size:", unsafe.Sizeof(ps)) //8
+
+	var f func() = nil
+	fmt.Println("f.size:", unsafe.Sizeof(f)) //8
+	var si []int = nil
+	//var si1 []int = nil
+	fmt.Println("si.size:", unsafe.Sizeof(si)) //24
+
+	var m map[string]int = nil
+	fmt.Println("m.size:", unsafe.Sizeof(m)) //8
+	var ii interface{} = nil
+	//var ii1 interface{} = nil
+	fmt.Println("ii.size:", unsafe.Sizeof(ii)) //16
+
+	//fmt.Println(i == si) //编译不通过
+	fmt.Println(i == ii) //fasle
+	//fmt.Println(si == si1) //编译不通过
+}
