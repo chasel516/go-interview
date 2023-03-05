c108b6c73be5fcbefb924a699fea8ae281af98f5
phper95 <1366295050@qq.com>
2023-03-06 06:04:54 +0800

    4-19

--- demo/4-17/len-cap/main.go
+++ demo/4-17/len-cap/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var a [5]int
@@ -9,4 +12,30 @@ func main() {
 	fmt.Println("b.len", len(b), "b.cap", cap(b))
 	b = append(b, 4)
 	fmt.Println("b.len", len(b), "b.cap", cap(b))
+
+	m := map[string]int{}
+	m["k1"] = 1
+	fmt.Println("m.len", len(m))
+
+	ch := make(chan int, 10)
+	fmt.Println("ch.len", len(ch), cap(ch))
+
+	//数组的长度和容量在编译阶段被估值
+	const arrLen = len(a)
+	const arrCap = cap(a)
+
+	//通过反射可以修改切片的长度
+	s := make([]string, 1, 6)
+
+	//注意，len要比cap小
+	reflect.ValueOf(&s).Elem().SetLen(2)
+	fmt.Println("s.len", len(s), "s.cap", cap(s))
+	//cap只能小于等于当前值
+	reflect.ValueOf(&s).Elem().SetCap(5)
+	fmt.Println("s.len", len(s), "s.cap", cap(s))
+
+	str := "go面试"
+	fmt.Println("str.len", len(str)) //unicode编码中，一个中文字符占3个字节
+	fmt.Println("str.runeLen", len([]rune(str)))
+
 }
---
+++ demo/4-19/pointer/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := new(int)
+	//y := *x
+	y := x
+	fmt.Println("x=", x, "y=", y)
+	tmp := 5
+	x = &tmp
+	fmt.Println("x=", x, *x, "y=", y, *y)
+
+	//a := new([3]int)
+	//fmt.Println(a)
+	//b := 3
+	//fmt.Println(&b)
+
+	//c := 1
+	//f1(c)
+	//fmt.Println("f1:c=", c) //c=1
+	//f2(&c)
+	//fmt.Println("f2:c=", c) //c=10
+
+	//i := int64(5)
+	//p := &i
+	//p++
+	//atomic.AddInt64(p, 1)
+
+	type Myint int32
+	type P1 *int32
+	type P2 *Myint
+
+	var x1 P1
+	var x2 *int32
+	x1 = x2 //隐式转换
+	fmt.Println(x1, x2)
+	x2 = x1 //隐式转换
+	var x3 *Myint
+
+	x4 := (*int32)(x3) //显示转换
+	x5 := (*Myint)(x2) //*Myint显示转换到*int32
+	fmt.Println(x4, x5)
+
+	var x6 P2
+	//x6 = x1           //P1不能直接隐式转换到P2,编译不通过
+	//x6 = (*Myint)(x1) //也不能直接显示转换，编译不通过
+	x6 = P2((*Myint)((*int32)(x1))) //P1经过3层显示转换到P2
+	fmt.Println(x6)
+
+	//fmt.Println(x1 == x2) // 可以比较
+	//fmt.Println(x2 == x3) //不能比较
+	//fmt.Println(x1 == x6) //不能比较
+	//fmt.Println(x2 == x6) //不能比较
+
+}
+
+func f1(x int) {
+	x = 10
+}
+
+func f2(x *int) {
+	//实参x的副本跟c指向同一个地址，所以修改副本指向的值也会影响到函数外c的值
+	*x = 10
+	//这里直接修改了实参x的副本地址，相当于让实参x指向了一个新的地址，所以这里改变x的地址并不会影响函数外c的值
+	x = nil
+}
