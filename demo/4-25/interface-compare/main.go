099c76bf5040d925eeabdad2e16e9b73cd1b1672
phper95 <2550702985@qq.com>
2023-03-08 20:06:40 +0800

    update

--- demo/4-25/interface-compare/main.go
+++ demo/4-25/interface-compare/main.go
@@ -10,15 +10,46 @@ type ReadErr struct {
 
 func main() {
 	err := read()
-	println(err)
+	fmt.Println(err)
 	if err != nil {
 		fmt.Println("has error")
 	} else {
 		fmt.Println("no error")
 	}
+
+	var a int
+	var b interface{} = a
+	var c int32
+	var d interface{} = c
+	println("b==d", b == d)
+
+	var e error
+	println("e:", e, "err:", err)
+
+	var emptyI interface{} // 空接口类型
+	// 非空接口类型
+	println("e = nil:", e == nil)
+	println("emptyI = nil:", emptyI == nil)
+	println("e :", e, "emptyI", emptyI)
+	println("e==emptyI", e == emptyI)
 }
 
 func read() error {
 	var err *ReadErr
 	return err
 }
+
+//type Interface1 interface {
+//	f1(string)
+//}
+//
+//type Interface2 interface {
+//	f1()
+//	f2()
+//}
+//
+//type Interface3 interface {
+//	Interface1
+//	Interface2
+//	f3()
+//}
