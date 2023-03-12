155cb8fe85dafb7ac569bfc60f7a1cc2d1cdc120
phper95 <1366295050@qq.com>
2023-03-12 23:47:52 +0800

    4-30

---
+++ demo/4-30/obj-compare/main.go
@@ -0,0 +1,48 @@
+package main
+
+func main() {
+	//var m map[string]int
+	//var n map[string]int
+
+	//fmt.Println(m == nil) //true
+	//fmt.Println(n == nil) //true
+	//不能通过编译
+	//fmt.Println(m ==n)
+
+	//m = make(map[string]int, 10)
+	//n = make(map[string]int, 100)
+	//for s, i := range m {
+	//	fmt.Println(s, i)
+	//}
+	//m["a"] = 1
+	//n["a"] = 1
+	//
+	//fmt.Println(reflect.DeepEqual(m, n)) //true
+
+	//type person struct {
+	//	name string
+	//}
+	//p1 := person{"test"}
+	//p2 := struct {
+	//	name string
+	//}{"test"}
+	//fmt.Println(p1 == p2)                  //true
+	//fmt.Println(reflect.DeepEqual(p1, p2)) //false
+
+	//x := new(int)
+	//y := new(int)
+	//fmt.Println("x==y:", x == y)                             //x==y: false
+	//fmt.Println("DeepEqual(x, y):", reflect.DeepEqual(x, y)) //DeepEqual(x, y): true
+
+	//type link struct {
+	//	data interface{}
+	//	next *link
+	//}
+	//var a, b, c link
+	//a.next = &b
+	//b.next = &c
+	//c.next = &a
+	//
+	//fmt.Println(a == b)                  //false
+	//fmt.Println(reflect.DeepEqual(a, b)) //true
+}
--- demo/4-32/for-range/main.go
+++ demo/4-32/for-range/main.go
@@ -1,28 +1,73 @@
 package main
 
-import "fmt"
-
-type St struct {
-	name string
-}
-
-func (s *St) Point() {
-	fmt.Println(s.name)
-}
-
-//func (s St) Point() {
-//	fmt.Println(s.name)
-//}
-
 func main() {
 
-	st := []St{
-		{"a"},
-		{"b"},
-		{"c"},
-	}
-	for _, t := range st {
-		//fmt.Println(reflect.TypeOf(t))
-		defer t.Point()
-	}
+	//错误写法
+	//slice := []int{0, 1, 2, 3}
+	//m := make(map[int]*int)
+	//for key, val := range slice {
+	//	m[key] = &val
+	//}
+
+	//0 -> 3
+	//1 -> 3
+	//2 -> 3
+	//3 -> 3
+	//for k, v := range m {
+	//	fmt.Println(k, "->", *v)
+	//}
+
+	////正确写法
+	//slice := []int{0, 1, 2, 3}
+	//m := make(map[int]*int)
+	//for key, val := range slice {
+	//	value := val
+	//	m[key] = &value
+	//}
+	//
+	////0 -> 0
+	////1 -> 1
+	////2 -> 2
+	////3 -> 3
+	//for k, v := range m {
+	//	fmt.Println(k, "->", *v)
+	//}
+
+	//slice := []int{0, 1, 2, 3}
+	//for _, val := range slice {
+	//	val++
+	//}
+	//fmt.Println(slice)
+	//
+	//arr := [3]int{1, 2, 3}
+	//for _, val := range arr {
+	//	val++
+	//}
+	//fmt.Println(arr)
+
+	//slice := []int{0, 1, 2, 3}
+	//m := make(map[int]*int)
+	//for key, _ := range slice {
+	//	m[key] = &slice[key]
+	//}
+	////0 -> 0
+	////1 -> 1
+	////2 -> 2
+	////3 -> 3
+	//for k, v := range m {
+	//	fmt.Println(k, "->", *v)
+	//}
+
+	//var ap *[3]int
+
+	////会导致panic
+	//for i, p := range ap {
+	//	fmt.Println(i, p)
+	//}
+	//
+	////舍弃for range的第二个值
+	////不会导致panic
+	//for i, _ := range ap {
+	//	fmt.Println(i)
+	//}
 }
