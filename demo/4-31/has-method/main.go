34c5a643de46feb6955e86fe13a0bebb68b3540c
phper95 <1366295050@qq.com>
2023-03-12 19:43:06 +0800

    4-31

---
+++ demo/4-31/has-method/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type person struct {
+	Name string
+}
+
+func (p *person) Say(str string) string {
+	return p.Name + str
+}
+
+func main() {
+	//方法1：通过类型转化判断
+	p := &person{}
+	fmt.Println(HasMethodSay(p)) //true
+
+	//方法2；通过反射实现
+	fmt.Println(HasMethod(p, "Say")) //true
+
+}
+
+func HasMethodSay(v interface{}) (has bool) {
+	_, has = v.(interface{ Say(str string) string })
+	return
+}
+
+func HasMethod(obj interface{}, methodName string) bool {
+	if methodName == "" {
+		return false
+	}
+	object := reflect.ValueOf(obj)
+	// 获取到方法
+	method := object.MethodByName(methodName)
+	if !method.IsValid() {
+		return false
+	}
+	return true
+}
