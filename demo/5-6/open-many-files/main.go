aa2d17ebb207060d162b9ca8689a1102bbc4dd37
phper95 <1366295050@qq.com>
2023-04-01 13:11:23 +0800

    5-7

--- demo/5-6/open-many-files/main.go
+++ demo/5-6/open-many-files/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"os"
+	"strconv"
+	"sync"
+)
+
 func main() {
 
+}
+
+func processFiles() error {
+	for i := 0; i < 100000; i++ {
+		file, err := os.Open("file" + strconv.Itoa(i) + ".txt")
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		// 处理文件内容
+		// ...
+	}
+	return nil
+}
+
+func processFiles1() error {
+	for i := 0; i < 100000; i++ {
+		processFile("file" + strconv.Itoa(i) + ".txt")
+	}
+	return nil
+}
+
+func processFile(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 处理文件内容
+	// ...
+	return nil
+}
+
+func processFiles2() error {
+	var wg sync.WaitGroup
+	for i := 0; i < 100000; i++ {
+		wg.Add(1)
+		go func(index int) {
+			defer wg.Done()
+			processFile("file" + strconv.Itoa(i) + ".txt")
+		}(i)
+	}
+	wg.Wait()
+	return nil
 }
--- demo/5-7/defer-misunderstanding/main.go
+++ demo/5-7/defer-misunderstanding/main.go
@@ -9,11 +9,16 @@ func (n *number) ptrprint() { fmt.Println(*n) }
 
 func main() {
 	//f1()
-	//f2()
-	fmt.Println(f3(2))
+	//fmt.Println(f2(2))
+	//f3()
+	//f4()
+	//f5()
+	//f6()
+	//fn()
+	f()
 }
 
-func f1() {
+func f() {
 	var n number
 
 	defer n.print()
@@ -21,10 +26,10 @@ func f1() {
 	defer func() { n.print() }()
 	defer func() { n.ptrprint() }()
 
-	n = 3
+	n = 123
 }
 
-func f2() {
+func f1() {
 	x := 0
 
 	defer fmt.Println(x)                    // 作为外部变量传递
@@ -35,10 +40,72 @@ func f2() {
 
 }
 
-func f3(x int) (r int) {
+func f2(x int) (r int) {
 	defer func() {
 		r += x // 修改返回值
 	}()
 
 	return x + x // <=> r = x + x; return
+}
+
+// defer中变量的估值时刻
+func f3() {
+	x := 0
+	defer fmt.Println(x)
+	x = 1
+	fmt.Println("done")
+}
+
+// defer中实参的估值时刻
+func f4() {
+	x := 0
+	defer func(paramx int) {
+		fmt.Println(paramx)
+	}(x)
+	x = 1
+	fmt.Println("done")
+}
+
+// defer中闭包的估值时刻
+func f5() {
+	x := 0
+	defer func() {
+		fmt.Println(x)
+	}()
+	x = 1
+	fmt.Println("done")
+}
+
+func f6() {
+	func() {
+		for x := 0; x < 3; x++ {
+			defer fmt.Println("x:", x)
+		}
+	}()
+
+	func() {
+		for y := 0; y < 3; y++ {
+			defer func() {
+				fmt.Println("y:", y)
+			}()
+		}
+	}()
+}
+
+func fn() int {
+	defer func(x int) {
+		fmt.Println("defer:", x)
+	}(fn1())
+	fmt.Println("fn")
+	return fn2()
+}
+
+func fn1() int {
+	fmt.Println("fn1")
+	return 1
+}
+
+func fn2() int {
+	fmt.Println("fn2")
+	return 2
 }
