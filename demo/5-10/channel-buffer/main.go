d584159cbb815b6b283e1d4482272ea23296f2e9
phper95 <1366295050@qq.com>
2023-04-04 08:25:10 +0800

    update

--- demo/5-10/channel-buffer/main.go
+++ demo/5-10/channel-buffer/main.go
@@ -14,15 +14,9 @@ func main() {
 	//f6()
 	//f7()
 	//f8()
-	f9()
-	//f10()
-	//var ch chan struct{}
-	//
-	//for i := 0; i < 3; i++ {
-	//	go test()
-	//}
-	//fmt.Println("永久阻塞")
-	//<-ch
+	//f9()
+	f10()
+	//f11()
 }
 
 func f1() {
@@ -69,7 +63,7 @@ func f6() {
 	<-ch
 }
 
-//协程中只写不读
+// 协程中只写不读
 func f7() {
 	ch := make(chan int)
 	go func() {
@@ -80,7 +74,7 @@ func f7() {
 
 }
 
-//协程中只读不写
+// 协程中只读不写
 func f8() {
 	ch := make(chan int)
 	go func() {
@@ -90,7 +84,6 @@ func f8() {
 
 }
 
-//当我们在
 func f9() {
 	ch := make(chan int)
 	ch1 := make(chan int)
@@ -103,21 +96,23 @@ func f9() {
 		}
 	}()
 
-	go func() {
-		for {
-			select {
-			case <-ch1:
-				ch <- 1
-			}
+	for {
+		select {
+		case <-ch1:
+			ch <- 1
 		}
-	}()
+	}
 
 }
 
 func f10() {
-	ch := make(chan int, 1)
-	ch <- 1
-	fmt.Println(<-ch)
+	var ch chan int
+	for i := 0; i < 3; i++ {
+		go test()
+	}
+	fmt.Println("永久阻塞")
+	<-ch
+	//ch <- 1
 }
 
 func test() {
@@ -125,3 +120,33 @@ func test() {
 		time.Sleep(time.Second)
 	}
 }
+
+func f11() {
+	ch := make(chan int, 1)
+	ch <- 1
+	fmt.Println(<-ch)
+}
+
+// 缓冲区已满，发送方阻塞，没有接收方而发生死锁
+func f12() {
+	ch := make(chan int, 1)
+	ch <- 1
+	ch <- 2
+}
+
+// // 缓冲区为空，接收方阻塞而无法开启新的发送方而导致死锁
+func f13() {
+	ch := make(chan int, 1)
+	<-ch
+	ch <- 1
+}
+
+// 缓冲区大小为1的channel中的两个goroutine相互等待
+func f14() {
+	ch := make(chan int, 1)
+	ch <- 1 // 发送方向缓冲区中放入数据
+	<-ch    // 接收方从缓冲区中取出数据
+	ch <- 2 // 发送方尝试向缓冲区中发送数据，但接收方已经退出，导致发送方阻塞
+}
+
+//在这个例子中，我们创建了一个大小为1的缓冲区，并向其中发送了一个数据，然后从中接收了一个数据。由于缓冲区大小为1，发送方尝试向缓冲区中发送数据时将被阻塞，而由于接收方已经退出，发送方也无法继续执行，从而导致死锁。
