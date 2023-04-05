26dafbfe869085f5fb94fa70cbc7b46342448580
phper95 <1366295050@qq.com>
2023-04-05 21:02:09 +0800

    5-12

--- demo/5-12/channel-closed/main.go
+++ demo/5-12/channel-closed/main.go
@@ -3,38 +3,70 @@ package main
 import (
 	"fmt"
 	"time"
+	"unsafe"
 )
 
 func main() {
-	channelIsNil()
 	//f1()
 	//f2()
 	//f3()
 	//f4()
 	//f5()
 	//f6()
-	f7()
+	//f7()
+	//f8()
+	//f9()
+	//f10()
 
-}
-func channelIsNil() {
-	var ch chan struct{}
-	var ch1 = make(chan struct{})
-	fmt.Println("ch == nil:", ch == nil)
-	fmt.Println("ch1 == nil:", ch1 == nil)
-	close(ch1)
-	fmt.Println("关闭后ch1 == nil:", ch1 == nil)
 }
 
 func f1() {
 	ch := make(chan int)
 	close(ch)
 	go func() {
-		<-ch
+		fmt.Println("开始接收数据")
+		fmt.Println("<-ch:", <-ch)
+		fmt.Println("数据接收完成")
 	}()
-	ch <- 1
+
+	go func() {
+		fmt.Println("开始发送数据")
+		ch <- 1
+		fmt.Println("发送数据完毕")
+	}()
+	time.Sleep(time.Second)
 }
 
 func f2() {
+	ch := make(chan int)
+	close(ch)
+	for {
+		select {
+		case c := <-ch:
+			fmt.Println("读取<-ch:", c)
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func f3() {
+	ch := make(chan int)
+	close(ch)
+	<-ch          //读取已经关闭的channel ，不会阻死
+	println("完成") //这句会输出
+}
+
+func f4() {
+	ch := make(chan int)
+	go func() {
+		close(ch)
+		println("已关闭ch") //这句会输出
+	}()
+	<-ch          //这里读取时也不会组塞住,会等到gorutine中关闭
+	println("完成") //这句会输出
+}
+
+func f5() {
 	ch := make(chan int)
 	go func() {
 		for i := 0; i < 5; i++ {
@@ -49,7 +81,7 @@ func f2() {
 	}
 }
 
-func f3() {
+func f6() {
 	ch := make(chan int)
 	go func() {
 		for i := 0; i < 5; i++ {
@@ -70,38 +102,45 @@ func f3() {
 	}
 }
 
-func f4() {
-	var c chan int
-	close(c) // panic: close of nil channel
-}
-
-func f5() {
-	c := make(chan int)
-	defer close(c)
-	go func() {
-		c <- 1
-		close(c)
-	}()
-
-	fmt.Println(<-c)
-
-}
-func f6() {
-	c := make(chan int)
-	defer close(c)
-	go func() {
-		c <- 1
-	}()
-
-	fmt.Println(<-c)
-}
-
+// 多次关掉 channel 会触发运行时错误
 func f7() {
 	c := make(chan int)
+	defer close(c)
+	go func() {
+		c <- 1
+		close(c)
+	}()
+	fmt.Println(<-c)
+}
+
+// 使用 defer 延迟关闭 channel，并且确保 channel 只释放一次
+func f8() {
+	c := make(chan int)
+	defer close(c)
+	go func() {
+		c <- 1
+	}()
+
+	fmt.Println(<-c)
+}
+
+// 关闭channel之后，channel中的数据依然是可以读取到
+func f9() {
+	c := make(chan int)
 	go func() {
 		c <- 1
 		close(c)
 	}()
 
 	fmt.Println(<-c)
+}
+
+func f10() {
+	arr := [4095]string{}
+	fmt.Println(unsafe.Sizeof(arr))
+	ch := make(chan [4095]string)
+	go func() {
+		ch <- arr
+	}()
+	time.Sleep(time.Second)
 }
