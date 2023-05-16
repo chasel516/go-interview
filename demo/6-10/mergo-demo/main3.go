23362c45c4398aae5a041e3576919d8eafb88928
phper95 <1366295050@qq.com>
2023-05-16 23:48:17 +0800

    update

--- demo/6-10/mergo-demo/main3.go
+++ demo/6-10/mergo-demo/main3.go
@@ -10,6 +10,9 @@ import (
 type timeTransformer struct {
 }
 
+type timeAddOneDayTransformer struct {
+}
+
 func init() {
 	//日志显示行号和文件名
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -17,6 +20,27 @@ func init() {
 
 // 自定义转换器
 func (t timeTransformer) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
+	if typ == reflect.TypeOf(time.Time{}) {
+		return func(dst, src reflect.Value) error {
+			if dst.CanSet() {
+				//通过反射值使用MethodByName调用IsZero方法，判断是否是字段的零值
+				isZero := dst.MethodByName("IsZero")
+
+				//获取IsZero方法的调用结果
+				result := isZero.Call([]reflect.Value{})
+
+				//如果目标段为零值则覆盖
+				if result[0].Bool() {
+					dst.Set(src)
+				}
+			}
+			return nil
+		}
+	}
+	return nil
+}
+
+func (t timeAddOneDayTransformer) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
 	if typ == reflect.TypeOf(time.Time{}) {
 		return func(dst, src reflect.Value) error {
 			if dst.CanSet() {
