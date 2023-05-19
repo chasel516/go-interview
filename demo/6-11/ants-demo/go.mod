d4245a38d5b84c5f2f621c61e9a6de3451d32f62
phper95 <1366295050@qq.com>
2023-05-19 08:27:46 +0800

    update

--- demo/6-11/ants-demo/go.mod
+++ demo/6-11/ants-demo/go.mod
@@ -4,7 +4,7 @@ go 1.20
 
 require (
 	github.com/panjf2000/ants/v2 v2.7.4
-	github.com/phper95/pkg/routine v0.0.0-20230517161021-ae2b8e3aba2b
+	github.com/phper95/pkg/routine v0.0.0-20230519000315-327130a12567
 )
 
 require (
--- demo/6-11/ants-demo/main2.go
+++ demo/6-11/ants-demo/main2.go
@@ -1,12 +1,34 @@
 package main
 
-import "github.com/phper95/pkg/routine"
+import (
+	"github.com/phper95/pkg/routine"
+	"log"
+	"sync/atomic"
+	"time"
+)
 
-const PollNameDefault = "default"
+const PoolNameTest = "test-pool"
 
+func init() {
+	routine.InitPoolWithName(PoolNameTest, 1, 5, 0)
+}
 func main() {
-	routine.InitPoolWithName(PollNameDefault, 1, 1, 0)
+
+	// 定义一个计数器，用于统计处理的任务数
+	var counter int64
 	for i := 0; i < 100; i++ {
-		routine.
+		x := i
+		routine.GetPool(PoolNameTest).Put(func() {
+			// 模拟任务处理时间
+			time.Sleep(time.Second)
+			log.Println("执行任务", x)
+			// 增加计数器的值
+			atomic.AddInt64(&counter, 1)
+		})
+		for routine.GetPool(PoolNameTest).QueueLen() > 0 {
+			time.Sleep(100 * time.Millisecond)
+		}
+		// 输出计数器的值
+		log.Println("counter:", counter)
 	}
 }
