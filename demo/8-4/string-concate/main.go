2adeed27de8f67e1af8fbdf629c576fcc1b45432
phper95 <2550702985@qq.com>
2023-08-29 07:12:12 +0800

    8-4

--- demo/8-4/string-concate/main.go
+++ demo/8-4/string-concate/main.go
@@ -7,34 +7,65 @@ import (
 )
 
 func main() {
-	//连接符 +
+	//1.连接符 +
 	s1 := "ab"
 	s2 := "cd"
-	fmt.Println(s1 + s2)
-	//字符串格式化函数fmt.Sprintf
+	fmt.Println("连接符 +:", s1+s2)
+
+	//2.字符串格式化函数fmt.Sprintf
 	s := fmt.Sprintf("%s%s", s1, s2)
 	fmt.Println("fmt.Sprintf:", s)
 
-	//bytes.Buffer
+	//3.预分配bytes.Buffer
 	var buf bytes.Buffer
+	buf.Grow(len(s1) + len(s2))
 	buf.WriteString(s1)
 	buf.WriteString(s2)
 	fmt.Println("bytes.Buffer:", buf.String())
+	BufferCapResize()
 
-	//strings.Builder
+	//4.预分配strings.Builder
 	var bu strings.Builder
+	bu.Grow(len(s1) + len(s2))
 	bu.WriteString(s1)
 	bu.WriteString(s2)
 	fmt.Println("strings.Buffer:", bu.String())
+	BuilderCapResize()
 
-	//预分配[]byte
+	//5.预分配[]byte
 	bt := make([]byte, 0, len(s1)+len(s2))
 	bt = append(bt, s1...)
 	bt = append(bt, s2...)
 	fmt.Println("预分配[]byte:", string(bt))
 
-	//strings.join
+	//6.strings.join
 	sl := []string{"ab", "cd"}
 	str := strings.Join(sl, "")
 	fmt.Println("strings.Join:", str)
+}
+func BufferCapResize() {
+	var str = "abcd"
+	var buf bytes.Buffer
+	cap := 0
+	for i := 0; i < 10000; i++ {
+		if buf.Cap() != cap {
+			fmt.Println(buf.Cap())
+			cap = buf.Cap()
+		}
+		buf.WriteString(str)
+	}
+}
+
+func BuilderCapResize() {
+	var str = "abcd"
+	var builder strings.Builder
+	cap := 0
+	for i := 0; i < 10000; i++ {
+		if builder.Cap() != cap {
+			fmt.Println(builder.Cap())
+			cap = builder.Cap()
+		}
+		builder.WriteString(str)
+
+	}
 }
---
+++ demo/8-4/string-concate/str_test.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+	"unsafe"
+)
+
+// 使用 + 拼接字符串
+func BenchmarkStringPlus(b *testing.B) {
+	result := ""
+	s := "Hello, World!"
+	for i := 0; i < b.N; i++ {
+		result += s
+	}
+	_ = result
+}
+
+func BenchmarkStringPlus2(b *testing.B) {
+	result := ""
+	const s = "Hello, World!"
+	for i := 0; i < b.N; i++ {
+		result += s
+	}
+	_ = result
+}
+
+func BenchmarkStringPlus3(b *testing.B) {
+	result := ""
+	const s1 = "Hello, "
+	const s2 = "World!"
+	for i := 0; i < b.N; i++ {
+		result = s1 + s2
+	}
+	_ = result
+}
+
+func BenchmarkStringPlus4(b *testing.B) {
+	result := ""
+	s1 := "Hello, "
+	s2 := "World!"
+	for i := 0; i < b.N; i++ {
+		result = s1 + s2
+	}
+	_ = result
+}
+
+// 使用 fmt.Sprintf 拼接字符串
+func BenchmarkStringFmtSprintf(b *testing.B) {
+	result := ""
+	for i := 0; i < b.N; i++ {
+		result = fmt.Sprintf("%s%s", result, "Hello, World!")
+	}
+	_ = result
+}
+
+// 使用 buffer.WriteString 拼接字符串
+func BenchmarkStringBufferWrite(b *testing.B) {
+	var buffer bytes.Buffer
+	for i := 0; i < b.N; i++ {
+		buffer.WriteString("Hello, World!")
+	}
+	result := buffer.String()
+	_ = result
+}
+
+func BenchmarkStringBufferWriteWithGrow(b *testing.B) {
+	var buffer bytes.Buffer
+	buffer.Grow(b.N * len("Hello, World!"))
+	for i := 0; i < b.N; i++ {
+		buffer.WriteString("Hello, World!")
+	}
+	result := buffer.String()
+	_ = result
+}
+
+// 使用 strings.Builder 拼接字符串
+func BenchmarkStringsBuilder(b *testing.B) {
+	var builder strings.Builder
+	for i := 0; i < b.N; i++ {
+		builder.WriteString("Hello, World!")
+	}
+	result := builder.String()
+	_ = result
+}
+
+func BenchmarkStringsBuilderWithGrow(b *testing.B) {
+	var builder strings.Builder
+	builder.Grow(b.N * len("Hello, World!"))
+	for i := 0; i < b.N; i++ {
+		builder.WriteString("Hello, World!")
+	}
+	result := builder.String()
+	_ = result
+}
+
+// 使用 []byte 拼接字符串
+func BenchmarkStringByte(b *testing.B) {
+	bt := make([]byte, 0, 0)
+	for i := 0; i < b.N; i++ {
+		bt = append(bt, "Hello, World!"...)
+	}
+	result := string(bt)
+	_ = result
+}
+
+func BenchmarkStringByteWithPre(b *testing.B) {
+	bt := make([]byte, 0, b.N*len("Hello, World!"))
+	for i := 0; i < b.N; i++ {
+		bt = append(bt, "Hello, World!"...)
+	}
+	result := string(bt)
+	_ = result
+}
+
+func BenchmarkStringByteWithPreAndZeroCopy(b *testing.B) {
+	bt := make([]byte, 0, b.N*len("Hello, World!"))
+	for i := 0; i < b.N; i++ {
+		bt = append(bt, "Hello, World!"...)
+	}
+	result := *(*string)(unsafe.Pointer(&bt))
+	_ = result
+}
+
+// 使用 strings.Join 拼接字符串
+func BenchmarkStringsJoin(b *testing.B) {
+	result := ""
+	for i := 0; i < b.N; i++ {
+		parts := []string{"Hello,", "World!"}
+		result = strings.Join(parts, " ")
+	}
+	_ = result
+}
