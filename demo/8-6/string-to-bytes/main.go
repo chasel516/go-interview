db3d7ca6868b6f29be7f1395997f454ae60a2be5
liyong9 <liyong9>
2023-08-31 21:28:32 +0800

    update

--- demo/8-6/string-to-bytes/main.go
+++ demo/8-6/string-to-bytes/main.go
@@ -11,20 +11,42 @@ func main() {
 	s := strings.Repeat("1", 1<<20)
 	ptr := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	fmt.Println("s pointer:", unsafe.Pointer(ptr.Data))
-	StringSlice(s)
-}
-func StringSlice(s string) {
-	s1 := []byte(s[:20])
-	ptr := (*reflect.StringHeader)(unsafe.Pointer(&s1))
-	//字符串字节数组的的地址
+
+	//转字节切片
+	s1 := []byte(s)
+	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s1))
 	fmt.Println("s1:", unsafe.Pointer(ptr.Data))
+
+	//强转
 	s2 := string(s1)
 	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s2))
-	//字符串字节数组的的地址
 	fmt.Println("s2:", unsafe.Pointer(ptr.Data))
 
 	s3 := *(*string)(unsafe.Pointer(&s1))
 	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s3))
-	//字符串字节数组的的地址
 	fmt.Println("s3:", unsafe.Pointer(ptr.Data))
+
+	//字节切片转字符串
+	s4 := string(s1)
+	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s4))
+	fmt.Println("s4:", unsafe.Pointer(ptr.Data))
+
+	s5 := *(*[]byte)(unsafe.Pointer(&s))
+	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s5))
+	fmt.Println("s5:", unsafe.Pointer(ptr.Data))
+
+	s6 := stringToBytes(s)
+	ptr = (*reflect.StringHeader)(unsafe.Pointer(&s6))
+	fmt.Println("s6:", unsafe.Pointer(ptr.Data))
+
+}
+
+func stringToBytes(s string) []byte {
+	strHeader := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	b := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: strHeader.Data,
+		Len:  strHeader.Len,
+		Cap:  strHeader.Len,
+	}))
+	return b
 }
