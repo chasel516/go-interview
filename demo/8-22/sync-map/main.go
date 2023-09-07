8d3fd0394a85f479f624829f76c63ad6537bc7a6
liyong9 <liyong9>
2023-09-07 19:29:20 +0800

    update

--- demo/8-22/sync-map/main.go
+++ demo/8-22/sync-map/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "sync"
 
+func main() {
+	wg := sync.WaitGroup{}
+	//m := make(map[int]int)
+	m := sync.Map{}
+	for i := 0; i < 10; i++ {
+		x := i
+		wg.Add(1)
+		go func() {
+			//m[x] = x
+			m.Store(x, x)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
