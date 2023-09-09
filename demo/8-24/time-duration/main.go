ce17d803dd60887d38db9ba68529a9501b9bf42e
phper95 <2550702985@qq.com>
2023-09-09 23:44:02 +0800

    8-24

--- demo/8-24/time-duration/main.go
+++ demo/8-24/time-duration/main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	//获取500ms后的执行戳，单位ms
+	afterUnixMilli1()
+	//afterUnixMilli2()
+}
 
-	start := time.Now().UnixMilli()
-	fmt.Println(start)
-	end := start + int64(time.Millisecond*500)
-	end1 := time.Duration(start) + time.Millisecond*500
+// 获取500ms后的时间戳，单位ms
+func afterUnixMilli1() {
+	nowUnixMilli := time.Date(2023, 10, 10, 0, 0, 0, 0, time.UTC).UnixMilli()
+	fmt.Println("afterUnixMilli1: now", nowUnixMilli)
+	//延迟后的时间戳 = 指定的时间戳+延时时间
+	end := nowUnixMilli + int64(time.Millisecond*500)
+	end1 := time.Duration(nowUnixMilli) + time.Millisecond*500
 	fmt.Println(end)
 	fmt.Println(int64(end1), end1)
+}
 
+// 获取500ms后的执行戳，单位ms
+func afterUnixMilli2() {
+	nowUnixMilli := time.Date(2023, 10, 10, 0, 0, 0, 0, time.UTC).UnixMilli()
+	end := nowUnixMilli + 500
+	delayduration := 500 * time.Millisecond
+	end1 := nowUnixMilli + delayduration.Milliseconds()
+	fmt.Println(end)
+	fmt.Println(end1)
 }
