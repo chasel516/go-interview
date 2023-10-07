b1ec5ad6684b7e9de0fd473a4c96ec448ae45eb9
unknown <2550702985@qq.com>
2023-10-07 22:27:58 +0800

    update

--- demo/9-1/concurrency-base/go.mod
+++ demo/9-1/concurrency-base/go.mod
@@ -1,5 +1,5 @@
 module concurrency-base
 
-go 1.21.0
+go 1.20
 
 require golang.org/x/sync v0.3.0
--- demo/9-5/atomic-lock/main.go
+++ demo/9-5/atomic-lock/main.go
@@ -2,33 +2,24 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"sync"
 	"time"
 
 	"golang.org/x/sync/semaphore"
 )
 
+func init() {
+	log.SetFlags(log.Ltime)
+}
 func main() {
-	sema := semaphore.NewWeighted(3)
-	ctx := context.Background()
-	for i := 0; i < 3; i++ {
-		x := i
-		sema.Acquire(ctx, 1)
-		go func() {
-			defer func() {
-				sema.Release(1)
-			}()
-			time.Sleep(time.Second)
-			fmt.Println(x)
-		}()
-	}
+	//waitGroup()
+	//semaphoreWait()
+	rateLimit()
 
-	//获取全部资源，获取不到阻塞
-	sema.Acquire(ctx, 3)
 }
 
-func waightGroup() {
+func waitGroup() {
 	wg := sync.WaitGroup{}
 	for i := 0; i < 3; i++ {
 		x := i
@@ -38,16 +29,16 @@ func waightGroup() {
 				wg.Done()
 			}()
 			time.Sleep(time.Second)
-			fmt.Println(x)
+			log.Println(x)
 		}()
 	}
 	wg.Wait()
 }
 
-func rateLimit() {
+func semaphoreWait() {
 	sema := semaphore.NewWeighted(3)
 	ctx := context.Background()
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 3; i++ {
 		x := i
 		sema.Acquire(ctx, 1)
 		go func() {
@@ -55,8 +46,30 @@ func rateLimit() {
 				sema.Release(1)
 			}()
 			time.Sleep(time.Second)
-			fmt.Println(x)
+			log.Println(x)
 		}()
 	}
 
+	//获取全部资源，获取不到阻塞
+	sema.Acquire(ctx, 3)
+}
+
+func rateLimit() {
+	sema := semaphore.NewWeighted(3)
+	wg := sync.WaitGroup{}
+	ctx := context.Background()
+	for i := 0; i < 100; i++ {
+		x := i
+		sema.Acquire(ctx, 1)
+		wg.Add(1)
+		go func() {
+			defer func() {
+				sema.Release(1)
+				wg.Done()
+			}()
+			time.Sleep(time.Second)
+			log.Println(x)
+		}()
+	}
+	wg.Wait()
 }
