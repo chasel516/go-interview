584414ce126acf5130c62c8759fcfd06a882dfc0
phper95 <2550702985@qq.com>
2023-10-30 23:22:16 +0800

    9-20

--- demo/9-20/concurency-mistaken1/main2.go
+++ demo/9-20/concurency-mistaken1/main2.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
-	start := time.Now()
-	timer := time.NewTimer(time.Second / 2)
-	select {
-	case <-timer.C:
-	default:
-		time.Sleep(time.Second) // 此分支被选中的可能性较大
-	}
-	timer.Reset(time.Second * 10) // 可能数据竞争
-	<-timer.C
-	fmt.Println(time.Since(start)) // 大约1s
+	lock := sync.Mutex{}
+	lock.Lock()
+	//lock.Unlock()
+	test(&lock)
+}
+func test(l *sync.Mutex) {
+	l.Lock()
 }
---
+++ demo/9-20/concurency-mistaken1/main3.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+func main() {
+	lock := sync.Mutex{}
+	lock.Lock()
+	test1(&lock)
+
+}
+func test1(l *sync.Mutex) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		test2(l)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+func test2(l *sync.Mutex) {
+	r := rand.Intn(10)
+	if r%2 == 0 {
+		return
+	}
+	doSth(l)
+	l.Unlock()
+}
+
+func doSth(l *sync.Mutex) {
+	l.Lock()
+	fmt.Println("doSth")
+}
