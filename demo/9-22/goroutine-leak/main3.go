ed8367398701eb5fc1043a588beb6501f4e77b2b
phper95 <2550702985@qq.com>
2023-11-03 06:30:07 +0800

    9-22

--- demo/9-22/goroutine-leak/main3.go
+++ demo/9-22/goroutine-leak/main3.go
@@ -8,25 +8,22 @@ import (
 )
 
 func init() {
-	go Ticker1(func() {
+	go Ticker2(func() {
 		log.Print("current go routine num: ", runtime.NumGoroutine())
 	}, time.Second)
 }
 
 func main() {
-	var ch chan int //只声明而没有通过make或者字面量的形式完成初始化
 	for i := 0; i < 100000; i++ {
 		go func(index int) {
-			//ch <- 1
-			<-ch
+			select {}
 			fmt.Println("end", index)
 		}(i)
 	}
-	//<-ch
-	ch <- 1
+	time.Sleep(time.Minute)
 }
 
-func Ticker1(f func(), d time.Duration) {
+func Ticker2(f func(), d time.Duration) {
 	ticker := time.NewTicker(d)
 	for {
 		select {
