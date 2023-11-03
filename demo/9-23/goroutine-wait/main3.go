54fccf1a01ba5f264cdcc602e2c497997762607b
liyong9 <liyong9>
2023-11-03 18:13:06 +0800

    update

--- demo/9-23/goroutine-wait/main3.go
+++ demo/9-23/goroutine-wait/main3.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func worker1(id int, done chan bool) {
+	// 执行协程任务
+	fmt.Printf("Worker %d done\n", id)
+	done <- true
+}
+
+func main() {
+	numWorkers := 5
+	done := make(chan bool, numWorkers)
+
+	for i := 1; i <= numWorkers; i++ {
+		go worker1(i, done)
+	}
+
+	for i := 0; i < numWorkers; i++ {
+		<-done
+	}
+
+	fmt.Println("All workers done")
+	close(done)
 }
--- demo/9-23/goroutine-wait/main4.go
+++ demo/9-23/goroutine-wait/main4.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	"golang.org/x/sync/semaphore"
+)
+
+func init() {
+	log.SetFlags(log.Ltime)
+}
+func main() {
+	sema := semaphore.NewWeighted(3)
+	ctx := context.Background()
+	for i := 0; i < 3; i++ {
+		x := i
+		sema.Acquire(ctx, 1)
+		go func() {
+			defer func() {
+				sema.Release(1)
+			}()
+			time.Sleep(time.Second)
+			log.Println(x)
+		}()
+	}
+
+	//获取全部资源，获取不到阻塞
+	sema.Acquire(ctx, 3)
 }
