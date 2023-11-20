26bd3f1e3905d8e2d965d6328971c7c87bbf2401
phper95 <2550702985@qq.com>
2023-11-21 06:11:56 +0800

    10-6

--- demo/10-6/godebug-demo/main.go
+++ demo/10-6/godebug-demo/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"runtime"
+	"sync"
+)
+
 func main() {
+	// 设置使用多个逻辑处理器
+	runtime.GOMAXPROCS(2)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// 第一个goroutine，被锁定在第一个逻辑处理器上
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10000000000000; i++ {
+			json.Marshal(i)
+		}
+	}()
+
+	// 启动更多goroutine
+	for {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10000000000000; i++ {
+				json.Marshal(i)
+			}
+		}()
+	}
 
 }
