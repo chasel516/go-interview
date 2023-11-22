c8fab41a5b6a2e7d24985b02b20f9f637e8d78ce
phper95 <2550702985@qq.com>
2023-11-22 08:30:21 +0800

    update

--- demo/10-3/benchmark-demo/pkg/demo_test.go
+++ demo/10-3/benchmark-demo/pkg/demo_test.go
@@ -2,22 +2,21 @@ package pkg
 
 import (
 	"testing"
-	"time"
 )
 
 func BenchmarkF1(b *testing.B) {
-	//b.N = 100
-	time.Sleep(time.Second)
+	b.N = 100
+	//time.Sleep(time.Second)
 	//b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		f1()
 	}
 }
 func BenchmarkF2(b *testing.B) {
-	//b.N = 100
-	b.StopTimer()
-	time.Sleep(time.Second)
-	b.StartTimer()
+	b.N = 100
+	//b.StopTimer()
+	//time.Sleep(time.Second)
+	//b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		f2()
 	}
